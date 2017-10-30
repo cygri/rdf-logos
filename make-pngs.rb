2c910cbd39ff12dec9e7bc8da1f00f2a19bd0201
Richard Cyganiak <richard@cyganiak.de>
2017-10-30 16:17:36 +0000

    Make script work better for Batik 1.9

--- make-pngs.rb
+++ make-pngs.rb
@@ -1,14 +1,15 @@
 #!/usr/bin/ruby
 if ARGV.length != 1 then
     puts "Usage: make-pngs batik_path"
-    puts "  where batik_path is the path to Apache Batik's main directory"
+    puts "  where batik_path is the directory containing Apache Batik's"
+    puts "  batik-rasterizer .jar file"
     exit
 end
 batik_path = ARGV[0]
 Dir['svg/*.svg'].each do |file|
     [40,48,64,96,128].each do |size|
         outfile = /svg\/(.*).svg/.match(file)[1] + "-#{size}.png"
-        cmd = "java -jar #{batik_path}/batik-rasterizer.jar -w #{size} -h #{size} -d png/#{outfile} #{file}"
+        cmd = "java -jar #{batik_path}/batik-rasterizer*.jar -w #{size} -h #{size} -d png/#{outfile} #{file}"
         system cmd
     end
 end
